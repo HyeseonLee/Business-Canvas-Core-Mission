d9dab880282be6b6000f2eec8b11855acfe40ab6
HyeseonLee <hyesun1999@gmail.com>
2023-11-24 19:28:51 +0900

    refactor: 중복되는 로직 함수로 분리

--- src/utils/readLocalStorage.ts
+++ src/utils/readLocalStorage.ts
@@ -2,33 +2,35 @@ import localforage from "localforage";
 import { BenchMarkInfo } from "../types/benchmark";
 import { v4 as uuidv4 } from "uuid";
 
-export async function readDefaultInfoFromLocalStorage() {
+async function getItemFromLocalForage() {
   const benchMarkInfo = await localforage.getItem("benchMark");
+  return benchMarkInfo ? (benchMarkInfo as BenchMarkInfo) : null;
+}
+
+export async function readDefaultInfoFromLocalStorage() {
+  const benchMarkInfo = await getItemFromLocalForage();
   if (benchMarkInfo == null) {
     return {
       title: "",
       description: "",
     };
   } else {
-    const wholeData = benchMarkInfo as BenchMarkInfo;
-    const { title, description } = wholeData;
+    const { title, description } = benchMarkInfo;
     return { title, description };
   }
 }
 
 export async function readSourceFromLocalStorage() {
-  const benchMarkInfo = await localforage.getItem("benchMark");
+  const benchMarkInfo = await getItemFromLocalForage();
   if (benchMarkInfo == null) {
     return [{ id: uuidv4(), title: "", url: "", dataArr: [] }];
   } else {
-    const wholeData = benchMarkInfo as BenchMarkInfo;
-    const { sources } = wholeData;
-    return sources;
+    return benchMarkInfo.sources;
   }
 }
 
 export async function readPreviewDataFromLocalStorage() {
-  const benchMarkInfo = await localforage.getItem("benchMark");
+  const benchMarkInfo = await getItemFromLocalForage();
   if (benchMarkInfo == null) {
     return {
       title: "",
@@ -48,8 +50,7 @@ export async function readPreviewDataFromLocalStorage() {
       ],
     };
   } else {
-    const wholeData = benchMarkInfo as BenchMarkInfo;
-    const { title, description, sources } = wholeData;
-    return { title, description, sources: sources };
+    const { title, description, sources } = benchMarkInfo;
+    return { title, description, sources };
   }
 }
